9d8e2b2d1e453c8ee2d9c76a0e20b825c675d0fc
Meowmeowzers <95127319+Meowmeowzers@users.noreply.github.com>
2022-08-07 01:12:12 +0800

    - Navigation bar titles
     added
    6 modified
     warning/s
     error/s

--- app/src/main/java/com/test/moneynote/database/Database.kt
+++ app/src/main/java/com/test/moneynote/database/Database.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [ListItem::class], version = 1, exportSchema = true)
+@Database(entities = [ListItem::class], version = 1, exportSchema = false)
 abstract class MainDatabase: RoomDatabase() {
 
     abstract val databaseDao: DatabaseDao
--- app/src/main/java/com/test/moneynote/database/DatabaseDao.kt
+++ app/src/main/java/com/test/moneynote/database/DatabaseDao.kt
@@ -1,10 +1,12 @@
 package com.test.moneynote.database
 
 import androidx.lifecycle.LiveData
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 
+@Dao
 interface DatabaseDao {
 
     @Insert
